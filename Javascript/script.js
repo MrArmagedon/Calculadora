162b1df1f24596518c5431977612ebe181a37712
MrArmagedon <evinent19876@iesjoanramis.org>
2024-10-14 10:42:41 +0200

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,36 @@
+
+function suma(){
+    let n1 = parseFloat(prompt("Tria un nombre"));
+    let n2 = parseFloat(prompt("Tria un altre nombre"));
+    document.write(n1+n2);
+}
+
+function resta(){
+    let n1 = parseFloat(prompt("Tria un nombre"));
+    let n2 = parseFloat(prompt("Tria un altre nombre"));
+    document.write(n1-n2);
+}
+
+function mult(){
+    let n1 = parseFloat(prompt("Tria un nombre"));
+    let n2 = parseFloat(prompt("Tria un altre nombre"));
+    document.write(n1*n2);
+}
+
+function divi(){
+    let n1 = parseFloat(prompt("Tria un nombre"));
+    let n2 = parseFloat(prompt("Tria un altre nombre"));
+    document.write(n1/n2);
+}
+
+function arrel2(){
+    let n1 = parseFloat(prompt("Tria un nombre"));
+    let n2 = parseFloat(prompt("Tria un altre nombre"));
+    document.write(Math.sqrt(n1));
+}
+
+function arrel3(){
+    let n1 = parseFloat(prompt("Tria un nombre"));
+    let n2 = parseFloat(prompt("Tria un altre nombre"));
+    document.write(Math.cbrt(n1));
+}
