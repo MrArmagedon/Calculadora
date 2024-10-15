df5dfbeafdede667c0a45b4782e2ee171ad3e898
MrArmagedon <evinent19876@iesjoanramis.org>
2024-10-15 10:45:33 +0200

    Add files via upload

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,36 +1,89 @@
 
+let n1, n2, operacio;
+
 function suma(){
-    let n1 = parseFloat(prompt("Tria un nombre"));
-    let n2 = parseFloat(prompt("Tria un altre nombre"));
-    document.write(n1+n2);
+    n1 = parseFloat(document.getElementById("pantalla").value)
+    operacio = "+"
+    document.getElementById("pantalla").value = ""
+    document.getElementById("pantalla").focus()
 }
 
 function resta(){
-    let n1 = parseFloat(prompt("Tria un nombre"));
-    let n2 = parseFloat(prompt("Tria un altre nombre"));
-    document.write(n1-n2);
+    n1 = parseFloat(document.getElementById("pantalla").value)
+    operacio = "-"
+    document.getElementById("pantalla").value = ""
+    document.getElementById("pantalla").focus()
 }
 
 function mult(){
-    let n1 = parseFloat(prompt("Tria un nombre"));
-    let n2 = parseFloat(prompt("Tria un altre nombre"));
-    document.write(n1*n2);
+    n1 = parseFloat(document.getElementById("pantalla").value)
+    operacio = "*"
+    document.getElementById("pantalla").value = ""
+    document.getElementById("pantalla").focus()
 }
 
 function divi(){
-    let n1 = parseFloat(prompt("Tria un nombre"));
-    let n2 = parseFloat(prompt("Tria un altre nombre"));
-    document.write(n1/n2);
+    n1 = parseFloat(document.getElementById("pantalla").value)
+    operacio = "/"
+    document.getElementById("pantalla").value = ""
+    document.getElementById("pantalla").focus()
 }
 
 function arrel2(){
-    let n1 = parseFloat(prompt("Tria un nombre"));
-    let n2 = parseFloat(prompt("Tria un altre nombre"));
-    document.write(Math.sqrt(n1));
+    n1 = parseFloat(document.getElementById("pantalla").value)
+    let resultat = Math.sqrt(n1)
+    document.getElementById("pantalla").value = resultat
+    document.getElementById("pantalla").focus()
 }
 
 function arrel3(){
-    let n1 = parseFloat(prompt("Tria un nombre"));
-    let n2 = parseFloat(prompt("Tria un altre nombre"));
-    document.write(Math.cbrt(n1));
+    n1 = parseFloat(document.getElementById("pantalla").value)
+    let resultat = Math.cbrt(n1)
+    document.getElementById("pantalla").value = resultat
+    document.getElementById("pantalla").focus()
+}
+
+function elev2(){
+    n1 = parseFloat(document.getElementById("pantalla").value)
+    let resultat = n1*n1
+    document.getElementById("pantalla").value = resultat
+    document.getElementById("pantalla").focus()
+}
+
+function elev3(){
+    n1 = parseFloat(document.getElementById("pantalla").value)
+    let resultat = n1*n1*n1
+    document.getElementById("pantalla").value = resultat
+    document.getElementById("pantalla").focus()
+}
+
+function calcula(){
+    let resultat;
+    n2 = parseFloat(document.getElementById("pantalla").value)
+   
+    if(operacio=="+"){
+        resultat = n1+n2
+        document.getElementById("pantalla").value = resultat
+    }
+
+    else if(operacio=="-"){
+        resultat = n1-n2
+        document.getElementById("pantalla").value = resultat
+
+    }
+
+    else if(operacio=="*"){
+        resultat = n1*n2
+        document.getElementById("pantalla").value = resultat
+
+    }
+
+    else if(operacio=="/"){
+        resultat = n1/n2
+        document.getElementById("pantalla").value = resultat
+
+    }
+
+    document.getElementById("pantalla").focus()
+
 }
